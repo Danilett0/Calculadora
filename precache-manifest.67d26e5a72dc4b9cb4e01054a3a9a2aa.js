2d2fc65490d1342aded38d169da41072cb451f5f
Danilett0 <daniilo.97@hotmail.com>
2023-06-05 22:18:47 -0500

    Updates

---
+++ precache-manifest.67d26e5a72dc4b9cb4e01054a3a9a2aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c335c32b941a4b045bb",
+    "url": "/static/css/main.2aaa095e.chunk.css"
+  },
+  {
+    "revision": "6c335c32b941a4b045bb",
+    "url": "/static/js/main.6c335c32.chunk.js"
+  },
+  {
+    "revision": "66e0c5001baabc17efbe",
+    "url": "/static/js/1.66e0c500.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "20fd190c6465db9dd32670aaae904c64",
+    "url": "/index.html"
+  }
+];
